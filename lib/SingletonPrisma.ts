d82efddcf322877b0a999ccde136a5932c147345
Murat Hayaloglu <murath31@hotmail.com>
2025-10-28 23:32:51 +0300

    prisma version, prisma singleton

--- lib/SingletonPrisma.ts
+++ lib/SingletonPrisma.ts
@@ -1,8 +1,14 @@
-import { PrismaClient } from '@/generated/prisma'
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient()
+  globalForPrisma.prisma ?? new PrismaClient({
+    log: ['error', 'warn'],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
